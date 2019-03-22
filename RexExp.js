bc2371897f2735f21176c72ce242a10c969e8869
GaneshPotharam <48698591+GaneshPotharam@users.noreply.github.com>
2019-03-22 15:19:47 -0700

    Add files via upload

---
+++ RexExp.js
@@ -0,0 +1,4 @@
+var str = "Iam Chethan";
+console.log(str.search('Chethan'));
+console.log(str.replace('Iam', 'Hai'));
+console.log(str.search(/chethan/i));
---
+++ stringOperations.js
@@ -0,0 +1,8 @@
+var string = "iam chethan";
+console.log(string.slice(4,11));// 1st parameter is index number , 2nd is length of the string 
+console.log(string.split(" "));//separates the string using given separator
+console.log(string.substr(3,7)); //1st and 2nd parameters are index numbers
+console.log(string.charAt(6)); // displays the character at given index number
+console.log(string.indexOf('t')); // displays the index number of given character
+console.log(string.concat(" Hello"));
+console.log(string.toUpperCase());
