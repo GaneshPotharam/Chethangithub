44d2ccf071949d4910ab9c0b9ab197a4b1b4bf53
GaneshPotharam <48698591+GaneshPotharam@users.noreply.github.com>
2019-03-22 11:55:04 -0700

    Update factorial.js

--- factorial.js
+++ factorial.js
@@ -1,10 +1,10 @@
 function factorialNum(number){
-    var i =1 , fact =1;
-    for(i ; i<=number; i++)
-    {
-        fact = fact * i;
+    if(number == 0){
+        return 1;
     }
-    return fact;
+    else{
+    return  number * factorialNum(number-1);
+}
 }
 var factorial = factorialNum(10);
 console.log(factorial);
