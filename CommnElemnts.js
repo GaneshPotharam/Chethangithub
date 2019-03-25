3862e0541c6cfa3dc2afff0325daa46d55d6fa5f
GaneshPotharam <48698591+GaneshPotharam@users.noreply.github.com>
2019-03-25 06:50:48 +0530

    Update CommnElemnts.js

--- CommnElemnts.js
+++ CommnElemnts.js
@@ -1,16 +1,12 @@
-var frstArray = new Array(2,3,5) ,
- scndArray = new Array (3,4,5),
- result = new Array();
-k=0;
-  console.log(frstArray);
-   console.log(scndArray);
-   for(var i = 0 ; i<frstArray.length ; i++){
-      
-       for(j =0 ; j<scndArray.length;j++){
-       if(frstArray[i] === scndArray[j]){ 
-       result[k] = scndArray[j];
-          k++;
-       }
-     }
-   }
-   console.log(result);
+function find_comnelem(farry , sarry){
+var result = [];
+ farry.forEach(function (elem1){
+    sarry.forEach(function (elem2){
+        if(elem1 === elem2){
+            result.push(elem1);
+        }
+    });
+ });
+ return result;
+}
+console.log(find_comnelem([3,4,5],[6,5,4]));
